b1266e34a91a2fdf527497a5e8939b76e28ef4ca
Dominic Buchheit <dominic.buchheit@gmail.com>
2023-11-14 13:40:38 +0100

    Removing existing approach, will be replaced

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,34 +1,5 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import SpinnerFullPage from "./components/SpinnerFullPage";
-import { GeolocationProvider } from "./contexts/GeolocationContext";
-
-// const Homepage = lazy(() => import("./pages/Homepage"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-const SelectView = lazy(() => import("./pages/SelectView"));
-const MapView = lazy(() => import("./pages/MapView"));
-const ListView = lazy(() => import("./pages/ListView"));
-
 function App() {
-  return (
-    <GeolocationProvider>
-      <BrowserRouter>
-        <Suspense fallback={<SpinnerFullPage />} />
-        <Routes>
-          {/* <Route index element={<Homepage />} /> */}
-          <Route index element={<ListView />} />
-          <Route path="app" element={<SelectView />} />
-          <Route path="map" element={<MapView />} />
-          <Route path="list" element={<ListView />} />
-          <Route path="new" element={null} />
-          <Route path="login" element={null} />
-          <Route path="user" element={null} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </GeolocationProvider>
-  );
+  return <h1>DropNote</h1>;
 }
 
 export default App;
