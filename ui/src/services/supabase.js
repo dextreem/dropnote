8bf7ed74f6bbb234321d11b92d2f048fd7e951d0
Dominic Buchheit <dominic.buchheit@gmail.com>
2024-08-28 20:48:30 +0200

    Moving supabase credentials to env file, adding an example env file

--- ui/src/services/supabase.js
+++ ui/src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://xxx.supabase.co";
-const supabaseKey = "xxx";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
