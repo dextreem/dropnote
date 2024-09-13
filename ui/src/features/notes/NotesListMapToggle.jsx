673fb28f2a12733872d5496abcce6740c962da5c
Dominic Buchheit <dominic.buchheit@gmail.com>
2024-09-13 22:09:37 +0200

    Dynamically show and change view buttons, based on media query breakpoints

--- ui/src/features/notes/NotesListMapToggle.jsx
+++ ui/src/features/notes/NotesListMapToggle.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Button from "../../components/Button";
 import { devices } from "../../styles/breakpoints";
+import useUiState, { NOTES_COMPONENTS } from "../../states/ui";
 
 const StyledNotesListMapToggle = styled.div`
   display: none;
@@ -17,10 +18,32 @@ const StyledButton = styled(Button)`
 `;
 
 function NotesListMapToggle() {
+  const listVisible = useUiState((state) =>
+    state.isVisible(NOTES_COMPONENTS.NOTES_LIST)
+  );
+  const mapVisible = useUiState((state) =>
+    state.isVisible(NOTES_COMPONENTS.NOTES_MAP)
+  );
+  const setVisible = useUiState((state) => state.setNoteVisibleComponent);
+
   return (
     <StyledNotesListMapToggle>
-      <StyledButton>Show As List</StyledButton>
-      <StyledButton>Show on Map</StyledButton>
+      {!listVisible && (
+        <StyledButton
+          variation="secondary"
+          onClick={() => setVisible(NOTES_COMPONENTS.NOTES_LIST)}
+        >
+          Show As List
+        </StyledButton>
+      )}
+      {!mapVisible && (
+        <StyledButton
+          variation="secondary"
+          onClick={() => setVisible(NOTES_COMPONENTS.NOTES_MAP)}
+        >
+          Show on Map
+        </StyledButton>
+      )}
     </StyledNotesListMapToggle>
   );
 }
